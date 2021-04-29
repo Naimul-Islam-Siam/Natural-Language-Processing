fe97d1ff778ed36413de13f6f6649576c1d55429
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-04-30 05:26:06 +0600

    Charting Word Length

--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/1. Regular expressions & word tokenization/Regular expressions & word tokenization.py
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/1. Regular expressions & word tokenization/Regular expressions & word tokenization.py
@@ -113,3 +113,28 @@ print(regexp_tokenize(german_text, capital_words))
 # Tokenize and print only emoji
 emoji = "['\U0001F300-\U0001F5FF'|'\U0001F600-\U0001F64F'|'\U0001F680-\U0001F6FF'|'\u2600-\u26FF\u2700-\u27BF']"
 print(regexp_tokenize(german_text, emoji))
+
+
+
+# ==================================================
+# --------- Charting Word Length with NLTK ---------
+# ==================================================
+
+# Split the script into lines: lines
+lines = holy_grail.split('\n')
+
+# Replace all script lines for speaker
+pattern = "[A-Z]{2,}(\s)?(#\d)?([A-Z]{2,})?:"
+lines = [re.sub(pattern, '', l) for l in lines]
+
+# Tokenize each line: tokenized_lines
+tokenized_lines = [regexp_tokenize(s, "\w+") for s in lines]
+
+# Make a frequency list of lengths: line_num_words
+line_num_words = [len(t_line) for t_line in tokenized_lines]
+
+# Plot a histogram of the line lengths
+plt.hist(line_num_words)
+
+# Show the plot
+plt.show()
