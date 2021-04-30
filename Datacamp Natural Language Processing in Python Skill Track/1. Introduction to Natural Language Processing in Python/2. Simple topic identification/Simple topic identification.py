adea5557c9795080b59af1c71d39c63d1dc7314a
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-05-01 04:13:29 +0600

    New Notes and Code: Bag of words

---
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/2. Simple topic identification/Simple topic identification.py
@@ -0,0 +1,19 @@
+# ====================================================
+# ------- Building a counter with bag of words -------
+# ====================================================
+
+
+# Import Counter
+from collections import Counter 
+
+# Tokenize the article: tokens
+tokens = word_tokenize(article)
+
+# Convert the tokens into lowercase: lower_tokens
+lower_tokens = [t.lower() for t in tokens]
+
+# Create a Counter with the lowercase tokens: bow_simple
+bow_simple = Counter(lower_tokens)
+
+# Print the 10 most common tokens
+print(bow_simple.most_common(10))
