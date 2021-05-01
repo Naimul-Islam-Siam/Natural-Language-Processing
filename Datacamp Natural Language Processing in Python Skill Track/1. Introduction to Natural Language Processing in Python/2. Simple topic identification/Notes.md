d45731981571e9b2dee136b5a148f8f4fa7ab951
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-05-01 07:24:42 +0600

    Genism

--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/2. Simple topic identification/Notes.md
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/2. Simple topic identification/Notes.md
@@ -9,3 +9,17 @@
 - Removing stop words and unwanted tokens
 - Good experiment with different approaches and then decide according to which works the best for your need
 
+## Gensim
+Uses top academic models for complex tasks
+- building a document or word vector
+- topic identification and document comparison
+
+**Word vectors** are multi-dimensional mathematical representations of words created using deep learning methods. They give us insight into relationships between words in a corpus.
+
+**Corpus or Corpora(Plural):** A set of texts used to help perform nlp tasks
+
+Genism uses a simple bag of words model which transforms each document into a bag of words using the token ids and the frequency of each tokens in the document.
+
+Genism corpus is a list of lists, each list item representing one document. Each document is now a series of tuples, the first item representing the tokenid from the dictionary and the second item representing the frequency of the token in the document.
+
+Unlike `Counter` based bag of words, `genism` models can easily be saved, updated and reused.
--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/2. Simple topic identification/Simple topic identification.py
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/2. Simple topic identification/Simple topic identification.py
@@ -44,3 +44,55 @@ bow = Counter(lemmatized)
 
 # Print the 10 most common tokens
 print(bow.most_common(10))
+
+
+
+# ==========================================================
+# ------- Creating and querying a corpus with gensim -------
+# ==========================================================
+
+# Import Dictionary
+from gensim.corpora.dictionary import Dictionary
+
+# Create a Dictionary from the articles: dictionary
+dictionary = Dictionary(articles)
+
+# Select the id for "computer": computer_id
+computer_id = dictionary.token2id.get("computer")
+
+# Use computer_id with the dictionary to print the word
+print(dictionary.get(computer_id))
+
+# Create a MmCorpus: corpus
+corpus = [dictionary.doc2bow(article) for article in articles]
+
+# Print the first 10 word ids with their frequency counts from the fifth document
+print(corpus[4][:10])
+
+
+
+# ===================================
+# ------- Gensim bag-of-words -------
+# ===================================
+
+# Save the fifth document: doc
+doc = corpus[4]
+
+# Sort the doc for frequency: bow_doc
+bow_doc = sorted(doc, key=lambda w: w[1], reverse=True)
+
+# Print the top 5 words of the document alongside the count
+for word_id, word_count in bow_doc[:5]:
+    print(dictionary.get(word_id), word_count)
+    
+# Create the defaultdict: total_word_count
+total_word_count = defaultdict(int)
+for word_id, word_count in itertools.chain.from_iterable(corpus):
+    total_word_count[word_id] += word_count
+    
+# Create a sorted list from the defaultdict: sorted_word_count
+sorted_word_count = sorted(total_word_count.items(), key=lambda w: w[1], reverse=True) 
+
+# Print the top 5 words across all documents alongside the count
+for word_id, word_count in sorted_word_count[:5]:
+    print(dictionary.get(word_id), word_count)
