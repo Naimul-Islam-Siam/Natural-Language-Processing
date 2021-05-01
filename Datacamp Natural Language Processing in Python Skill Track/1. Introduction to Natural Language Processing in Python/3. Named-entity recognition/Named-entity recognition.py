21fbdb6b76f2dbb6d6517051c623379d64ac0dd2
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-05-02 05:51:40 +0600

    SpaCy

--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/3. Named-entity recognition/Named-entity recognition.py
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/3. Named-entity recognition/Named-entity recognition.py
@@ -46,3 +46,22 @@ plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=140)
 
 # Display the chart
 plt.show()
+
+
+
+# ===============================================
+# -------- Comparing NLTK with spaCy NER --------
+# ===============================================
+
+# Import spacy
+import spacy
+
+# Instantiate the English model: nlp
+nlp = spacy.load('en', tagger=False, parser=False, matcher=False)
+
+# Create a new document: doc
+doc = nlp(article)
+
+# Print all of the found entities and their labels
+for ent in doc.ents:
+    print(ent.label_, ent.text)
--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/3. Named-entity recognition/Notes.md
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/3. Named-entity recognition/Notes.md
@@ -3,3 +3,11 @@
   * Important Entities are: People, Places, Dates, Organizations, Works of art etc
 - Can be used alongsided topic identification
 - Answers basic questions like who, when, where, what
+
+## SpaCy
+- Similar to `gensim` but focuses on creating nlp pipelines to generate models and corpora
+
+Why use `SpaCy` for NER
+- Easy Pipeline creation
+- Different entity types compared to `nltk`
+- Easily finds entities in tweets and chat messages
