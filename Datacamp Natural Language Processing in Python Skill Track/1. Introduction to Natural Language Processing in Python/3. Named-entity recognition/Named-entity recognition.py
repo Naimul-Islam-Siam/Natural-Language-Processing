efb4e5116843cdf03fcc6e99aafbc1d4ff4eb4fe
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-05-02 06:05:05 +0600

    Polyglot

--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/3. Named-entity recognition/Named-entity recognition.py
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/3. Named-entity recognition/Named-entity recognition.py
@@ -65,3 +65,42 @@ doc = nlp(article)
 # Print all of the found entities and their labels
 for ent in doc.ents:
     print(ent.label_, ent.text)
+
+
+
+# ==========================================
+# -------- French NER with polyglot --------
+# ==========================================
+
+# Create a new text object using Polyglot's Text class: txt
+txt = Text(article)
+
+# Print each of the entities found
+for ent in txt.entities:
+    print(ent)
+    
+# Print the type of ent
+print(type(ent))
+
+# Create the list of tuples: entities
+entities = [(ent.tag, ' '.join(ent)) for ent in txt.entities]
+
+# Print entities
+print(entities)
+
+# Initialize the count variable: count
+count = 0
+
+# Iterate over all the entities
+for ent in txt.entities:
+    # Check whether the entity contains 'Márquez' or 'Gabo'
+    if "Márquez" in ent or "Gabo" in ent:
+        # Increment count
+        count += 1
+
+# Print count
+print(count)
+
+# Calculate the percentage of entities that refer to "Gabo": percentage
+percentage = count / len(txt.entities)
+print(percentage)
--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/3. Named-entity recognition/Notes.md
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/3. Named-entity recognition/Notes.md
@@ -4,6 +4,7 @@
 - Can be used alongsided topic identification
 - Answers basic questions like who, when, where, what
 
+
 ## SpaCy
 - Similar to `gensim` but focuses on creating nlp pipelines to generate models and corpora
 
@@ -11,3 +12,8 @@ Why use `SpaCy` for NER
 - Easy Pipeline creation
 - Different entity types compared to `nltk`
 - Easily finds entities in tweets and chat messages
+
+
+## polyglot
+- Uses word vectors
+- Supports more than 130 languages
