f74959a0c4dbb058a924a0460a4c4e3fbbd13a5b
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-05-03 23:48:09 +0600

    Train and test using CountVectorizer

--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/4. Building a fake news classifier/Building a fake news classifier.py
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/4. Building a fake news classifier/Building a fake news classifier.py
@@ -75,3 +75,30 @@ print(difference)
 
 # Check whether the DataFrames are equal
 print(count_df.equals(tfidf_df))
+
+
+
+# =================================================================================
+# -------- Training and testing the "fake news" model with CountVectorizer --------
+# =================================================================================
+
+# Import the necessary modules
+from sklearn import metrics
+from sklearn.naive_bayes import MultinomialNB
+
+# Instantiate a Multinomial Naive Bayes classifier: nb_classifier
+nb_classifier = MultinomialNB()
+
+# Fit the classifier to the training data
+nb_classifier.fit(count_train, y_train)
+
+# Create the predicted tags: pred
+pred = nb_classifier.predict(count_test)
+
+# Calculate the accuracy score: score
+score = metrics.accuracy_score(y_test, pred)
+print(score)
+
+# Calculate the confusion matrix: cm
+cm = metrics.confusion_matrix(y_test, pred, labels=["FAKE", "REAL"])
+print(cm)
