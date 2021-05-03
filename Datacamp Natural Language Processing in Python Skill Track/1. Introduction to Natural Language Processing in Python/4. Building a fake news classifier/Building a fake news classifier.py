c898f367a6876242562d45e2dff0fe5fb328eaa5
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-05-04 00:03:40 +0600

    Improve your model

--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/4. Building a fake news classifier/Building a fake news classifier.py
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/4. Building a fake news classifier/Building a fake news classifier.py
@@ -125,3 +125,46 @@ print(score)
 # Calculate the confusion matrix: cm
 cm = metrics.confusion_matrix(y_test, pred, labels=['FAKE', 'REAL'])
 print(cm)
+
+
+
+# ======================================
+# -------- Improving your model --------
+# ======================================
+
+# Create the list of alphas: alphas
+alphas = np.arange(0, 1, 0.1)
+
+# Define train_and_predict()
+def train_and_predict(alpha):
+    # Instantiate the classifier: nb_classifier
+    nb_classifier = MultinomialNB(alpha=alpha)
+    # Fit to the training data
+    nb_classifier.fit(tfidf_train, y_train)
+    # Predict the labels: pred
+    pred = nb_classifier.predict(tfidf_test)
+    # Compute accuracy: score
+    score = metrics.accuracy_score(y_test, pred)
+    return score
+
+# Iterate over the alphas and print the corresponding score
+for alpha in alphas:
+    print('Alpha: ', alpha)
+    print('Score: ', train_and_predict(alpha))
+    print()
+
+
+# Get the class labels: class_labels
+class_labels = nb_classifier.classes_
+
+# Extract the features: feature_names
+feature_names = tfidf_vectorizer.get_feature_names()
+
+# Zip the feature names together with the coefficient array and sort by weights: feat_with_weights
+feat_with_weights = sorted(zip(nb_classifier.coef_[0], feature_names))
+
+# Print the first class label and the top 20 feat_with_weights entries
+print(class_labels[0], feat_with_weights[:20])
+
+# Print the second class label and the bottom 20 feat_with_weights entries
+print(class_labels[1], feat_with_weights[-20:])
--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/4. Building a fake news classifier/Notes.md
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/4. Building a fake news classifier/Notes.md
@@ -2,3 +2,9 @@
 - Commonly used for testing nlp classification problems
 - Given a particular piece of data, you likely is a particular outcome
 - Not always the best tool for a job, but simple and effective enough for a simple classification
+
+## Improving the model
+- Tweaking alpha levels
+- Trying a new classification model
+- Training on a larger dataset
+- Improving text preprocessing
