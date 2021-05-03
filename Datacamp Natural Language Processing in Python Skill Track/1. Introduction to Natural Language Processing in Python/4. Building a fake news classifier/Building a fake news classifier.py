313a50aecc035527e230cff41b94823fa7a2503f
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2021-05-03 23:52:42 +0600

    Train and test using TfidfVectorizer

--- Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/4. Building a fake news classifier/Building a fake news classifier.py
+++ Datacamp Natural Language Processing in Python Skill Track/1. Introduction to Natural Language Processing in Python/4. Building a fake news classifier/Building a fake news classifier.py
@@ -102,3 +102,26 @@ print(score)
 # Calculate the confusion matrix: cm
 cm = metrics.confusion_matrix(y_test, pred, labels=["FAKE", "REAL"])
 print(cm)
+
+
+
+# =================================================================================
+# -------- Training and testing the "fake news" model with TfidfVectorizer --------
+# =================================================================================
+
+# Create a Multinomial Naive Bayes classifier: nb_classifier
+nb_classifier = MultinomialNB()
+
+# Fit the classifier to the training data
+nb_classifier.fit(tfidf_train, y_train)
+
+# Create the predicted tags: pred
+pred = nb_classifier.predict(tfidf_test)
+
+# Calculate the accuracy score: score
+score = metrics.accuracy_score(y_test, pred)
+print(score)
+
+# Calculate the confusion matrix: cm
+cm = metrics.confusion_matrix(y_test, pred, labels=['FAKE', 'REAL'])
+print(cm)
